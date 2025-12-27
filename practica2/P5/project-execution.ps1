9d6dadb7f1fd4bc8aafd566a485151ebf92bf18e
Alejandro <deolozaga2004@gmail.com>
2025-12-27 15:52:14 +0000

    update: changed scripts
    Changed project cleanUp and execution scripts. Added CLI messages.

--- practica2/P5/project-execution.ps1
+++ practica2/P5/project-execution.ps1
@@ -1,7 +1,15 @@
+Write-Host "INICIANDO EJECUCION"
+
+Write-Host "Esperando a que Firehose termine..."
+Start-Sleep -Seconds 60
+Write-Host "Ejecutando crawler durante 90 segundos..."
+
 aws glue start-crawler --name energy-raw-crawler
 
 Start-Sleep -Seconds 90
 
-Write-host "Ejecutando los trabajos de AWS GLUE..."
+Write-Host "Ejecutando AWS Glue Jobs..."
 aws glue start-job-run --job-name energy-daily-aggregation
 aws glue start-job-run --job-name energy-monthly-aggregation
+
+Write-Host "EJECUCION COMPLETADA"
